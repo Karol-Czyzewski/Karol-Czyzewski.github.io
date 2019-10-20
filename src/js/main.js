240b6bd582b514b6faf952b6b759ac5959f0aae8
Karol Czyżewski <Karolek0493@wp.pl>
2019-10-20 23:30:13 +0200

    add: github api

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
+const list = document.querySelector('.projects__list-js');
 
+fetch("https://api.github.com/users/Karol-Czyzewski/repos?sort=update")
+.then(resp => resp.json())
+.then (resp => {
+const repos = resp
+    for (const repo of repos) {
+        list.innerHTML += `<li><a href=${repo.html_url}>${repo.name}</a></li>`; 
+    }
+})
+.catch(err => {
+    console.log(err); 
+})
