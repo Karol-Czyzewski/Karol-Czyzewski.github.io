7c56c05d4e03ce1890758b366b336998fc737f69
Karol Czyżewski <Karolek0493@wp.pl>
2019-10-26 23:10:34 +0200

    add new err & grid in ul

--- src/js/main.js
+++ src/js/main.js
@@ -12,5 +12,5 @@ const repos = resp
 })
 .catch(err => {
     console.log(err); 
-    list.innerHTML += "Nie moża wyświetliść API :) Sprawdź swoje połączenie z internetem";
+    list.innerHTML += `<p class="err-js">"Nie moża wyświetliść API :) Sprawdź swoje połączenie z internetem"</p>`;
 })
